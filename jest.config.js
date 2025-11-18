a4b4c91d2d4362af0b073514d38a89e8e9e21f60
Taufiq <taufiqelrahman65@gmail.com>
2025-11-18 18:54:21 +0700

    refactor: update test command to allow passing with no tests and adjust Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,7 @@ const customJestConfig = {
     "!**/node_modules/**",
     "!**/.next/**",
   ],
+  testRegex: undefined,
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 };
 
