b9f3c75c7463ce92c48a743e12e2f69c041c6d95
Taufiq El Rahman <taufiq.elrahman@gojek.com>
2025-11-18 19:01:16 +0700

    refactor: update test command to use Jest directly and enable passWithNoTests option

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ const customJestConfig = {
     "!**/node_modules/**",
     "!**/.next/**",
   ],
-  testRegex: undefined,
+  passWithNoTests: true,
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 };
 
