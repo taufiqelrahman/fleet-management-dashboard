61e624bfb9be040a600ad290d279390013d1fe95
Taufiq <taufiqelrahman65@gmail.com>
2025-12-20 08:32:28 +0700

    fix: correct regex for base64 URL decoding in PushNotificationToggle

--- components/push-notification-toggle.tsx
+++ components/push-notification-toggle.tsx
@@ -59,7 +59,7 @@ export function PushNotificationToggle({
   const urlBase64ToUint8Array = (base64String: string) => {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding)
-      .replace(/\\-/g, "+")
+      .replace(/-/g, "+")
       .replace(/_/g, "/");
 
     const rawData = window.atob(base64);
